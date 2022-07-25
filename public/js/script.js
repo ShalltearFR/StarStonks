9c169ed5ed44080edf6594c57f54a24a575836f3
unknown <Jossuha@hotmail.fr>
2022-07-26 00:38:03 +0200

    Ajout menu hamburger sans animation

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,26 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    console.log("www JS imported successfully!");
+// document.addEventListener(
+//   "DOMContentLoaded",
+//   () => {
+//     console.log("www JS imported successfully!");
+//   },
+//   false
+// );
+
+const $hamburger = document.querySelector("#hamburger")
+const $nav = document.querySelector("nav")
+let isHamburgerOpen = false
+let lockMenu = false
+
+$hamburger.addEventListener( "click", () => {
+    if (!isHamburgerOpen && !lockMenu){ //Déroule le menu
+      $nav.style.paddingBottom = "300px"
+      $nav.style.filter = "grayscale(75%)";
+      isHamburgerOpen = true
+    }else if (isHamburgerOpen && !lockMenu){ //Range le menu
+      $nav.style.paddingBottom = "0px"
+      $nav.style.filter = "";
+      isHamburgerOpen = false
+    }
   },
   false
 );
