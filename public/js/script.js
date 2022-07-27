e3ab700d5808d76aee7c65ef0cc4f34860efd382
unknown <Jossuha@hotmail.fr>
2022-07-27 23:46:41 +0200

    Animation nav

--- public/js/script.js
+++ public/js/script.js
@@ -10,20 +10,56 @@ const $hamburger = document.querySelector("#hamburger")
 const $nav = document.querySelector("nav")
 let isHamburgerOpen = false
 let lockMenu = false
+let hamburgerInterval
 
 $hamburger.addEventListener( "click", () => {
     if (!isHamburgerOpen && !lockMenu){ //Déroule le menu
-      $nav.style.paddingBottom = "300px"
+      //$nav.style.paddingBottom = "300px"
       $nav.style.filter = "grayscale(75%)";
+      hamburgerInterval = setInterval(()=>{hamburgerAnimation("open")},35)
+      lockMenu = true
       isHamburgerOpen = true
     }else if (isHamburgerOpen && !lockMenu){ //Range le menu
-      $nav.style.paddingBottom = "0px"
+      //$nav.style.paddingBottom = "0px"
       $nav.style.filter = "";
+      hamburgerInterval = setInterval(()=>{hamburgerAnimation("close")},35)
+      lockMenu = true
       isHamburgerOpen = false
     }
   },
   false
 );
+
+function hamburgerAnimation(status){ // Animation d'ouverture/fermeture du menu
+  const padding = (function(){
+    let toNumber = ""
+    for (let i = 0; i < $nav.style.paddingBottom.length - 2; i++){
+      toNumber = `${toNumber}${$nav.style.paddingBottom[i]}`
+    }
+    return Number(toNumber)
+  })()
+
+    if (status === "open"){
+      if (padding < 300){
+        $nav.style.paddingBottom = `${padding + 40}px`
+      } else{
+        clearInterval(hamburgerInterval)
+        $nav.style.borderRadius = "35px"
+        lockMenu = false
+      }
+    }
+
+    if (status === "close"){
+      if (padding > 0){
+        $nav.style.paddingBottom = `${padding - 40}px`
+      } else{
+        clearInterval(hamburgerInterval)
+        $nav.style.borderRadius = "0px"
+        lockMenu = false
+      }
+    }
+  console.log(padding)
+}
 
 
 let isNavBackground = false
