148398675c0093b416ff9140a15c296c0cf0814b
Benjamin Amzzough <amzzough.benjamin@yahoo.fr>
2022-07-30 15:10:26 +0200

    cart.hbs

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,3 +1,6 @@
+const Ticket = require("../models/Ticket.model");
+const Trip = require("../models/Trip.model");
+
 const router = require("express").Router();
 
 /* GET home page */
@@ -21,5 +24,26 @@ router.get("/classes", (req, res, next) => {
     user: req.session.user,
   });
 });
+// router.get("/cart", (req, res, next) => {
+//   const 
+//   Trip
+//     .populate("from")
+//     .populate("to")
+//     .findOne({req.cart.from.name})
+//     .then(LocationFromDB => {
+//       res.render("cart", {
+//         title: "Panier",
+//         user: req.session.user,
+//         cart: req.session.cart,
+//         location: LocationFromDB
+//       })
+//     })
+//     .catch(err => {
+//       console.log("Index.routes.js res.render Location error ", err);
+//       next(err);
+//     })
+
+// });
+
 
 module.exports = router;
