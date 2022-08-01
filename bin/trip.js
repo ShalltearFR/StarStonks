60a7f28d6e26197b956cdb87edf3384600e93c7c
Benjamin Amzzough <amzzough.benjamin@yahoo.fr>
2022-08-01 20:22:24 +0200

    bin/trip&css

--- bin/trip.js
+++ bin/trip.js
@@ -18,68 +18,68 @@ const Trip = require("../models/Trip.model");
 const data = [
   {
     from: "62dee3cb8d13f46f3bc96d16", // Mustafar
-    to :"62dee3cb8d13f46f3bc96d13", // Terre
+    to: "62dee3cb8d13f46f3bc96d13", // Terre
     date: "2022-07-01T20:30:00Z",
     arrived: "2022-07-02T10:30:00Z",
-    base_price :{
-        value: 150000
+    base_price: {
+      value: 150000
     },
-    class : "gold"
+    class: "gold"
   },
   {
     from: "62dee3cb8d13f46f3bc96d16",
-    to :"62dee3cb8d13f46f3bc96d13",
+    to: "62dee3cb8d13f46f3bc96d13",
     date: "2022-07-02T21:30:00Z",
     hours: "22h30",
     arrived: "2022-07-03T07:25:00Z",
-    base_price :{
-        value: 165000
+    base_price: {
+      value: 165000
     },
-    class : "gold"
+    class: "gold"
   },
   {
     from: "62dee3cb8d13f46f3bc96d16",
-    to :"62dee3cb8d13f46f3bc96d13",
+    to: "62dee3cb8d13f46f3bc96d13",
     date: "2022-06-29T18:15:00Z",
     arrived: "2022-06-30T15:45:00Z",
-    base_price :{
-        value: 130000
+    base_price: {
+      value: 130000
     },
-    class : "silver"
+    class: "silver"
   },
   {
     from: "62dee3cb8d13f46f3bc96d16",
-    to :"62dee3cb8d13f46f3bc96d13",
+    to: "62dee3cb8d13f46f3bc96d13",
     date: "2022-07-08T22:30:00Z",
     arrived: "2022-07-10T03:10:00Z",
-    base_price :{
-        value: 115000
+    base_price: {
+      value: 115000
     },
-    class : "bronze"
+    class: "bronze"
   },
   {
     from: "62dee3cb8d13f46f3bc96d16",
-    to :"62dee3cb8d13f46f3bc96d13",
+    to: "62dee3cb8d13f46f3bc96d13",
     date: "2022-07-08T20:30:00Z",
     arrived: "2022-07-10T22:30:00Z",
-    base_price :{
-        value: 116000
+    base_price: {
+      value: 116000
     },
-    class : "bronze"
+    class: "bronze"
   },
   {
     from: "62dee3cb8d13f46f3bc96d16", // Mustafar
-    to :"62dee3cb8d13f46f3bc96d13", // Terre
+    to: "62dee3cb8d13f46f3bc96d13", // Terre
     date: "2022-07-01T22:30:00Z",
     arrived: "2022-07-02T05:30:00Z",
-    base_price :{
-        value: 150000
+    base_price: {
+      value: 150000
     },
-    class : "gold"
+    class: "gold"
   },
 ];
 Trip.create(data)
-.then(()=>{
+  .then(() => {
     console.log("Importation réussi")
     mongoose.disconnect()
-});
+  });
