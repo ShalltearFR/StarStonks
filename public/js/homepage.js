af75d60ab30492a40c04854a4fa7238042ef150e
unknown <Jossuha@hotmail.fr>
2022-08-03 22:07:40 +0200

    animation button homepage

---
+++ public/js/homepage.js
@@ -0,0 +1,21 @@
+$button = document.querySelector("#sendResult")
+$button.addEventListener("click", sendResult, false)
+
+waitingSound = new Audio("https://res.cloudinary.com/shalltear/video/upload/v1659553383/Project%202/public/Star_Stonks_sonnerie2v2_g2ibaq.mp3")
+
+$popUpWaiting = document.querySelector("#popUpWaiting")
+$form = document.querySelector("form")
+
+function sendResult(){
+    const from = document.querySelector("#from").value
+    const to = document.querySelector("#to").value
+    
+    $form.style.display = "none"
+    $popUpWaiting.style.display = "flex"
+    waitingSound.play()
+
+    setTimeout(()=>{
+        window.location.assign(`/results?from=${from}&to=${to}`)
+    },3000)
+    //window.location.assign(`/results?from=${from}&to=${to}`);
+}
